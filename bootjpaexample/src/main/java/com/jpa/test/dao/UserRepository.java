e30cefc807dceba3033d543c653115ff821e3545
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-11-02 17:57:53 +0530

    Update UserRepository.java

--- bootjpaexample/src/main/java/com/jpa/test/dao/UserRepository.java
+++ bootjpaexample/src/main/java/com/jpa/test/dao/UserRepository.java
@@ -10,13 +10,13 @@ public interface UserRepository extends CrudRepository<User, Integer>{
  
 	public List<User> findByName(String name);
 	public List<User> findByNameAndCity(String name , String City);
-	
+	//Using JPQL Query
 	@Query("select u FROM User u")
 	public List<User> getAllUser();
 	
 	@Query("select u FROM User u WHERE u.name= :n")
 	public List<User> getUserByName(@Param("n")String name);
-	
+	//Native Query
 	@Query(value = "select * FROM User" , nativeQuery = true)
 	public List<User> getUser();
 	
